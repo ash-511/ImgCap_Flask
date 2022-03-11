c188ce7195cef73a1249c068d3b3abfa0a60f6e0
ash-511 <aishwarya51100@gmail.com>
2022-03-11 16:01:00 +0530

    Changed app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def upload():
 
 
 if __name__ == "__main__":
-    app.run(host = '0.0.0.0', port = port, debug=False)
+    app.run(host = '0.0.0.0', port = port, debug=True)
 
 
 # @app.route('/predict', methods=['POST'])
