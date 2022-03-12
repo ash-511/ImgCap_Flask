6cc1c5bbbad8a8a6cab32ecc76864b50b4de13c9
ash-511 <aishwarya51100@gmail.com>
2022-03-12 23:27:59 +0530

    Changes

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request, jsonify
 from werkzeug.utils import secure_filename
 from PIL import Image
 import pickle
-from sklearn.externals import joblib
 import os
 
 port = int(os.environ.get('PORT', 5000))
